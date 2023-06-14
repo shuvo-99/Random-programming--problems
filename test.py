224ae2c6000ffae01f42c476d6dbfac1a1b1f8c4
Md. Anonto Shuvo <anontoshuvo99@gmail.com>
2023-06-14 19:44:33 +0600

    Update test.py

--- test.py
+++ test.py
@@ -2588,56 +2588,148 @@
 
 
 # Summer final 2022
-class User:
-  activities = ["Post", "Like", "Comment"]
-  def __init__(self, name,email):
-    self.name = name
-    self.email = email
-  def UserActivity(self, activityType):
-    if activityType in User.activities:
-      return True
-    else:
-      return False
-  def userDetail(self):
-    return f"User Detail:\nName:{self.name}\nEmail: {self.email}"
+# class User:
+#   activities = ["Post", "Like", "Comment"]
+#   def __init__(self, name,email):
+#     self.name = name
+#     self.email = email
+#   def UserActivity(self, activityType):
+#     if activityType in User.activities:
+#       return True
+#     else:
+#       return False
+#   def userDetail(self):
+#     return f"User Detail:\nName:{self.name}\nEmail: {self.email}"
   
-class BracbookUser(User):
-  activity=['No recent acitivity.']
+# class BracbookUser(User):
+#   activity=['No recent acitivity.']
   
-  def __init__(self, name, email, phn='No set'):
-    super().__init__(name, email)
-    self.phn = phn
-    self.task=''
+#   def __init__(self, name, email, phn='No set'):
+#     super().__init__(name, email)
+#     self.phn = phn
+#     self.task=''
 
-  def userDetail(self):
-    return f"User Detail:\nName:{self.name}\nEmail: {self.email}\nPhone: {self.phn}\nActivity Log: {BracbookUser.activity[0]}"
+#   def userDetail(self):
+#     return f"User Detail:\nName:{self.name}\nEmail: {self.email}\nPhone: {self.phn}\nActivity Log: {BracbookUser.activity[0]}"
 
-  def UserActivity(self, activityType):
-    self.activityType = activityType
-    if super().UserActivity(activityType) == True:
-      self.task += self.activityType + ', '
-      print(f'{self.name} has {self.activityType}(d/ed) successfully')
-      BracbookUser.activity[0] = self.task[:-2]
-    else:
-      print(f'No activities found like {self.activityType}')
+#   def UserActivity(self, activityType):
+#     self.activityType = activityType
+#     if super().UserActivity(activityType) == True:
+#       self.task += self.activityType + ', '
+#       print(f'{self.name} has {self.activityType}(d/ed) successfully')
+#       BracbookUser.activity[0] = self.task[:-2]
+#     else:
+#       print(f'No activities found like {self.activityType}')
 
 
-user1 = BracbookUser("Rakait","xyz@gmail.com")
-print("1===========================")
-print(user1.userDetail())
-print("2===========================")
-user2 = BracbookUser("Sazzad","abc@gmail.com","01727xxxxxx")
-print("3===========================")
-print(user2.userDetail())
-print("4===========================")
-user1.UserActivity("Like")
-print("5===========================")
-user1.UserActivity("Comment")
-print("6===========================")
-print(user1.userDetail())
-print("7===========================")
-user2.UserActivity("Share")
-print("8===========================")
-user2.UserActivity("Comment")
-print("9===========================")
-print(user2.userDetail())
+# user1 = BracbookUser("Rakait","xyz@gmail.com")
+# print("1===========================")
+# print(user1.userDetail())
+# print("2===========================")
+# user2 = BracbookUser("Sazzad","abc@gmail.com","01727xxxxxx")
+# print("3===========================")
+# print(user2.userDetail())
+# print("4===========================")
+# user1.UserActivity("Like")
+# print("5===========================")
+# user1.UserActivity("Comment")
+# print("6===========================")
+# print(user1.userDetail())
+# print("7===========================")
+# user2.UserActivity("Share")
+# print("8===========================")
+# user2.UserActivity("Comment")
+# print("9===========================")
+# print(user2.userDetail())
+
+# -------------------- SUMMER 2023 -----------------------
+
+# Lab 2 Home Task - Tick Tac Toe
+
+# from IPython.display import clear_output
+# import time
+
+# # All the variables needed
+# board = [[1,2,3],[4,5,6],[7,8,9]] # The players will be placing their characters in this list
+# # and the whole game be conducted based on the current status of this list.
+# current_player_char = 'X' # The character of the current player
+# next_player_char = 'O' # The character of the next player
+# totalInputs = 9 # Since there are 9 slots in total in the whole board.
+# winner = None
+
+# def checkHorizontal():
+#     global winner
+#     for row in board:
+#         if row[0] == row[1] == row[2]:
+#             winner = "Player 1" if row[0] == 'X' else "Player 2"
+
+# def checkVertical():
+#     global winner
+#     for col in range(3):
+#         if board[0][col] == board[1][col] == board[2][col]:
+#             winner = "Player 1" if board[0][col] == 'X' else "Player 2"
+
+# def checkDiagonal():
+#     global winner
+#     if board[0][0] == board[1][1] == board[2][2] or board[0][2] == board[1][1] == board[2][0]:
+#         winner = "Player 1" if board[1][1] == 'X' else "Player 2"
+
+# def checkBoard():
+#     checkHorizontal()
+#     checkVertical()
+#     checkDiagonal()
+#     return winner is not None
+
+# def placeCharacterOnBoard(pos):
+#     if pos>=1 and pos<=9:
+#       row = (pos - 1) // 3
+#       col = (pos - 1) % 3
+
+#       if isinstance(board[row][col], int):
+#           board[row][col] = current_player_char
+#           return 1
+#       else:
+#           print("Invalid position. Please choose another position.")
+#           return 0
+#     else:
+#       print("Invalid position. Please choose another position.")
+#       return 0
+
+# # This function prints the current status of the 'board' list in a particular format.
+# def printCurrentBoard():
+#     print("-------------")
+#     for eachRow in board:
+#         print("|",end="")
+#         for eachCol in eachRow:
+#             print(f" {eachCol} ",end="|")
+#         print() #To move to the next row
+#         print("-------------")
+
+# # This function will simulate the whole game.
+# def runGame():
+#     global current_player_char, next_player_char, winner
+#     welcome_msg = '''Welcome to the TIC-TAC-TOE game. The first player to place a character on the board will be Player 1 (Character X) and the other player will be Player 2 (Character O).'''
+#     print(welcome_msg)
+#     inputCount = 0 # This variable is for counting the number of inputs and controlling the loop based on it.
+#     while inputCount < totalInputs:
+#         printCurrentBoard() # This function prints the current state of the board as a player needs to see it before making the next move.
+#         position = int(input(f"Player {(inputCount%2)+1}, enter a position that does not contain any X or O: ")) # The user needs to enter a position that does not contain any X or O.
+#         validityOfInput = placeCharacterOnBoard(position) # validityOfInput will be 1 if the user inputs a valid "position"; otherwise, it will be 0.
+#         inputCount += validityOfInput # If the player inputs a valid "position", the inputCount will increase by one; otherwise, it will remain as it is.
+#         if inputCount >= 5: # There is no need to check the board before 5 valid inputs as there will be no winners before 5 valid inputs.
+#             if checkBoard(): # If checkBoard() returns True, then the current player won the game.
+#                 winner = "Player 1" if current_player_char == 'X' else "Player 2"
+#                 clear_output() # This function clears the output panel.
+#                 break
+#         if validityOfInput: # Only if the player inputs a valid "position", the player characters will be swapped for the next move.
+#             current_player_char, next_player_char = next_player_char, current_player_char # The players will place characters alternatively. So if X is the current player's character, in the next turn O will be the current player's character.
+#         clear_output() # This function clears the output panel before printing the current board status for the next player.
+#     printCurrentBoard()
+#     # After the while loop, if the value of winner is still None, that means the game ended in a draw; otherwise, we have a winner.
+#     if winner is not None:
+#         print(f"Well done, {winner}. You have won the game.")
+#     else:
+#         print("The game ended in a draw.")
+
+# runGame()
+
