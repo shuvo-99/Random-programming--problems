be99003123e983bb1b979173a5f722a2b6d593eb
Md. Anonto Shuvo <anontoshuvo99@gmail.com>
2023-07-04 00:33:32 +0600

    Update test.py

--- test.py
+++ test.py
@@ -3056,3 +3056,54 @@
 # print("=================================")
 # a2.printDetail()
 # print("=================================")
+
+# Task 5
+
+class  TaxiLagbe:
+  def __init__(self,id,location):
+    self.id=id
+    self.location=location
+    self.names=[]
+    self.ticket_num=0
+    self.ticket_price=[]
+
+  def addPassenger(self,*args):
+    self.args=args
+    for i in args:
+        self.name, self.ticket_price = i.split('_')
+        if len(self.names)<4:
+            self.names.append(self.name)
+            self.ticket_num+=int(self.ticket_price)
+            print(f'Dear {self.name}! Welcome to TaxiLagbe.')
+        else:
+            print('Taxi Full! No more passengers can be added.')
+
+
+  def printDetails(self):
+    res=''
+    print(f'Trip info for Taxi number {self.id}')
+    print(f'This taxi can cover only {self.location} area.')
+    print(f'Total passengers: {len(self.names)}')
+    for i in self.names:
+        res+=i+', '
+    print(res[:-2])
+    print(f'Total collected fare: {self.ticket_num} Taka')
+
+
+taxi1 = TaxiLagbe('1010-01', 'Dhaka')
+print('-------------------------------')
+taxi1.addPassenger('Walker_100', 'Wood_200')
+taxi1.addPassenger('Matt_100')
+taxi1.addPassenger('Wilson_105')
+print('-------------------------------')
+taxi1.printDetails()
+print('-------------------------------')
+taxi1.addPassenger('Karen_200')
+print('-------------------------------')
+taxi1.printDetails()
+print('-------------------------------')
+taxi2 = TaxiLagbe('1010-02', 'Khulna')
+taxi2.addPassenger('Ronald_115')
+taxi2.addPassenger('Parker_215')
+print('-------------------------------')
+taxi2.printDetails()
