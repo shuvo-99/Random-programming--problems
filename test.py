8d27ca0058e587ddac41cfbd77d49ed98d2bf363
Md. Anonto Shuvo <anontoshuvo99@gmail.com>
2023-07-03 22:44:47 +0600

    Update test.py

--- test.py
+++ test.py
@@ -2883,43 +2883,43 @@
 # ASSIGNMENT 4 Summer 2023
 
 #Task 1
-class Customer:
-    def __init__(self):
-        print('Welcome to ABC Memorial Park')
-        self.count = 0
-        self.price = 0
+# class Customer:
+#     def __init__(self):
+#         print('Welcome to ABC Memorial Park')
+#         self.count = 0
+#         self.price = 0
         
-    def buyTicket(self,name,age):
-        self.count+=1
-        self.name = name
-        self.age = age
-        if self.count <=3: 
-            print(f'Successfully purchased a ticket for {self.name}!')
-            if self.age >10:
-                self.price += 100
-            else:
-                self.price += 50
-        else:
-            print("You can't buy more than 3 tickets")
+#     def buyTicket(self,name,age):
+#         self.count+=1
+#         self.name = name
+#         self.age = age
+#         if self.count <=3: 
+#             print(f'Successfully purchased a ticket for {self.name}!')
+#             if self.age >10:
+#                 self.price += 100
+#             else:
+#                 self.price += 50
+#         else:
+#             print("You can't buy more than 3 tickets")
         
-    def showDetails(self):
-        print(f'Amount of tickets: {self.count}')
-        print(f'Total price: {self.price} Taka')
+#     def showDetails(self):
+#         print(f'Amount of tickets: {self.count}')
+#         print(f'Total price: {self.price} Taka')
         
         
-print('1-------------------------') 
-customer1 = Customer() 
-print('2-------------------------') 
-customer1.buyTicket('Bob', 23) 
-customer1.buyTicket('Henry', 7) 
-customer1.buyTicket('Alexa', 30) 
-customer1.buyTicket('Jonas', 43) 
-print('3-------------------------') 
-customer1.showDetails() 
-print('4-------------------------') 
-customer2 = Customer() 
-print('5-------------------------') 
-customer2.buyTicket('Harry', 60) 
-customer2.buyTicket('Tomas', 28) 
-print('6-------------------------') 
-customer2.showDetails()
+# print('1-------------------------') 
+# customer1 = Customer() 
+# print('2-------------------------') 
+# customer1.buyTicket('Bob', 23) 
+# customer1.buyTicket('Henry', 7) 
+# customer1.buyTicket('Alexa', 30) 
+# customer1.buyTicket('Jonas', 43) 
+# print('3-------------------------') 
+# customer1.showDetails() 
+# print('4-------------------------') 
+# customer2 = Customer() 
+# print('5-------------------------') 
+# customer2.buyTicket('Harry', 60) 
+# customer2.buyTicket('Tomas', 28) 
+# print('6-------------------------') 
+# customer2.showDetails()
