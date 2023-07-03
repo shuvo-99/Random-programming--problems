e73cefefd0bdaa3ae78c954bd8f25b4e5c00a7d0
Md. Anonto Shuvo <anontoshuvo99@gmail.com>
2023-07-03 22:12:55 +0600

    Update test.py

--- test.py
+++ test.py
@@ -2832,49 +2832,51 @@
 
 # TASK-5
 
-class Order:
-  def __init__(self, menu,item_string):
-    self.menu = menu
-    self.item_string = item_string
-    self.item_list = self.item_string.split(', ')
-    self.items=[]
-    for i in self.item_list:
-      name, quan = i.split('-')
-      for k,v in self.menu.items():
-        if name==k:
-          price = int(quan)*v
-          self.items.append(name)
-          self.items.append(int(quan))
-          self.items.append(price)
+# class Order:
+#   def __init__(self, menu,item_string):
+#     self.menu = menu
+#     self.item_string = item_string
+#     self.item_list = self.item_string.split(', ')
+#     self.items=[]
+#     for i in self.item_list:
+#       name, quan = i.split('-')
+#       for k,v in self.menu.items():
+#         if name==k:
+#           price = int(quan)*v
+#           self.items.append(name)
+#           self.items.append(int(quan))
+#           self.items.append(price)
 
-menu = {
-    'Chicken_Cheeseburger' : 249,
-    'Mega_Cheeseburger' : 289,
-    'Fries' : 139,
-    'Hot_Wings' : 99,
-    'Rice_Bowl' : 299,
-    'Soft_Drinks' : 50
-}
+# menu = {
+#     'Chicken_Cheeseburger' : 249,
+#     'Mega_Cheeseburger' : 289,
+#     'Fries' : 139,
+#     'Hot_Wings' : 99,
+#     'Rice_Bowl' : 299,
+#     'Soft_Drinks' : 50
+# }
 
-order1 = Order(menu, "Chicken_Cheeseburger-2, Fries-3, Soft_Drinks-3")
-print(order1.items)
-print()
+# order1 = Order(menu, "Chicken_Cheeseburger-2, Fries-3, Soft_Drinks-3")
+# print(order1.items)
+# print()
 
-print('-'*35)
-print('Item           x Quantity :   Price')
-print('--------------   --------   -------')
+# print('-'*35)
+# print('Item           x Quantity :   Price')
+# print('--------------   --------   -------')
 
-index = 0
-total = 0
-while index < len(order1.items):
-  item = order1.items[index]
-  quantity = order1.items[index+1]
-  price = order1.items[index+2]
+# index = 0
+# total = 0
+# while index < len(order1.items):
+#   item = order1.items[index]
+#   quantity = order1.items[index+1]
+#   price = order1.items[index+2]
 
-  print(f'{item:20} x {quantity:2} : {price:7.2f}')
-  total += price
-  index += 3 # Going to next item
+#   print(f'{item:20} x {quantity:2} : {price:7.2f}')
+#   total += price
+#   index += 3 # Going to next item
 
-print('-'*35)
-print(f'Total:                      {total:7.2f}')
-print('-'*35)
+# print('-'*35)
+# print(f'Total:                      {total:7.2f}')
+# print('-'*35)
+
+
