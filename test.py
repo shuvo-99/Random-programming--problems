06839915da69207f1f42feaf8febc56de11b3bb1
Md. Anonto Shuvo <anontoshuvo99@gmail.com>
2023-07-04 00:19:55 +0600

    Update test.py

--- test.py
+++ test.py
@@ -2967,49 +2967,49 @@
 
 # Task 3
 
-class Student:
-    def __init__(self, name, cgpa, credit=9, dept='CSE'):
-        self.name = name
-        self.cgpa = cgpa
-        self.credit = credit
-        self.dept = dept
-        self.status = ''
+# class Student:
+#     def __init__(self, name, cgpa, credit=9, dept='CSE'):
+#         self.name = name
+#         self.cgpa = cgpa
+#         self.credit = credit
+#         self.dept = dept
+#         self.status = ''
         
-    def checkScholarshipEligibility(self):
-        if self.cgpa>=3.5 and self.credit>10:
-            if self.cgpa>=3.7:
-                self.status = 'Merit-based scholarship'
-                print(f'{self.name} is eligible for {self.status}.')
-            elif self.cgpa>=3.5 and self.cgpa<3.7:
-                self.status = 'Need-based scholarship'
-                print(f'{self.name} is eligible for {self.status}.')
-        else:
-            self.status = 'No scholarship'
-            print(f'{self.name} is not eligible for scholarship.')
+#     def checkScholarshipEligibility(self):
+#         if self.cgpa>=3.5 and self.credit>10:
+#             if self.cgpa>=3.7:
+#                 self.status = 'Merit-based scholarship'
+#                 print(f'{self.name} is eligible for {self.status}.')
+#             elif self.cgpa>=3.5 and self.cgpa<3.7:
+#                 self.status = 'Need-based scholarship'
+#                 print(f'{self.name} is eligible for {self.status}.')
+#         else:
+#             self.status = 'No scholarship'
+#             print(f'{self.name} is not eligible for scholarship.')
     
-    def showDetails(self):
-        print(f'Name: {self.name} \nDepartment: {self.dept} \nCGPA: {self.cgpa} \nNumber of Credits: {self.credit} \nScholarship Status: {self.status}')
+#     def showDetails(self):
+#         print(f'Name: {self.name} \nDepartment: {self.dept} \nCGPA: {self.cgpa} \nNumber of Credits: {self.credit} \nScholarship Status: {self.status}')
         
-print('--------------------------')
-std1 = Student("Alif",3.99,12)
-print('--------------------------')
-std1.checkScholarshipEligibility()
-print('--------------------------')
-std1.showDetails()
-print('--------------------------')
-std2 = Student("Mim",3.4)
-std3 = Student("Henry",3.5,15,"BBA")
-print('--------------------------')
-std2.checkScholarshipEligibility()
-print('--------------------------')
-std3.checkScholarshipEligibility()
-print('--------------------------')
-std2.showDetails()
-print('--------------------------')
-std3.showDetails()
-print('--------------------------')
-std4 = Student("Bob",4,6,"CSE")
-print('--------------------------')
-std4.checkScholarshipEligibility()
-print('--------------------------')
-std4.showDetails()
+# print('--------------------------')
+# std1 = Student("Alif",3.99,12)
+# print('--------------------------')
+# std1.checkScholarshipEligibility()
+# print('--------------------------')
+# std1.showDetails()
+# print('--------------------------')
+# std2 = Student("Mim",3.4)
+# std3 = Student("Henry",3.5,15,"BBA")
+# print('--------------------------')
+# std2.checkScholarshipEligibility()
+# print('--------------------------')
+# std3.checkScholarshipEligibility()
+# print('--------------------------')
+# std2.showDetails()
+# print('--------------------------')
+# std3.showDetails()
+# print('--------------------------')
+# std4 = Student("Bob",4,6,"CSE")
+# print('--------------------------')
+# std4.checkScholarshipEligibility()
+# print('--------------------------')
+# std4.showDetails()
