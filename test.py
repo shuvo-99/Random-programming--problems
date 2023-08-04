c2155b263f13fdc16050ced24c6a5f13f2a22a37
Md. Anonto Shuvo <anontoshuvo99@gmail.com>
2023-08-04 21:13:03 +0600

    Update test.py

--- test.py
+++ test.py
@@ -3709,51 +3709,53 @@
 
 
 # TASK 2
-class Spaceship:
-    def __init__(self,name, capacity):
-        self.name = name
-        self.capacity = capacity
-        self.cargoList = []
-        self.current_weight = 0
+# class Spaceship:
+#     def __init__(self,name, capacity):
+#         self.name = name
+#         self.capacity = capacity
+#         self.cargoList = []
+#         self.current_weight = 0
 
-    def load_cargo(self,obj):
-        if (self.current_weight + obj.weight) > self.capacity:
-            print(f'Warning: Unable to load Neutronium inside {self.name}. Exceeds capacity by {(self.current_weight + obj.weight) - self.capacity}.')
-        else:
-            self.current_weight += obj.weight
-            self.cargoList.append(obj.cargo_name)
+#     def load_cargo(self,obj):
+#         if (self.current_weight + obj.weight) > self.capacity:
+#             print(f'Warning: Unable to load Neutronium inside {self.name}. Exceeds capacity by {(self.current_weight + obj.weight) - self.capacity}.')
+#         else:
+#             self.current_weight += obj.weight
+#             self.cargoList.append(obj.cargo_name)
         
-    def display_details(self):
-        print(f'Spaceship Name: {self.name} \nCapacity: {self.capacity} \nCurrent Cargo Weight: {self.current_weight} \nCargo: {self.cargoList}')
+#     def display_details(self):
+#         print(f'Spaceship Name: {self.name} \nCapacity: {self.capacity} \nCurrent Cargo Weight: {self.current_weight} \nCargo: {self.cargoList}')
 
-class Cargo:
-    def __init__(self,cargo_name, weight):
-        self.cargo_name = cargo_name
-        self.weight = weight
+# class Cargo:
+#     def __init__(self,cargo_name, weight):
+#         self.cargo_name = cargo_name
+#         self.weight = weight
 
-# Creating spaceships
-falcon = Spaceship("Falcon", 50000)
-apollo = Spaceship("Apollo", 100000)
-enterprise = Spaceship("Enterprise", 220000)
-print("1.===================================")
-# Creating cargo
-gold = Cargo("Gold", 20000)
-platinum = Cargo("Platinum", 25000)
-dilithium = Cargo("Dilithium", 50000)
-trilithium = Cargo("Trilithium", 70000)
-neutronium = Cargo("Neutronium", 80000)
-print("2.===================================")
-# Loading cargo onto spaceships
-falcon.load_cargo(gold)
-falcon.load_cargo(platinum)
-falcon.display_details()
-print("3.===================================")
-apollo.load_cargo(gold)  # Apollo will not reach its total capacity
-apollo.display_details()
-print("4.===================================")
-falcon.load_cargo(neutronium)  # This should exceed Falcon's capacity
-print("5.===================================")
-enterprise.load_cargo(dilithium)
-enterprise.load_cargo(trilithium)
-enterprise.load_cargo(neutronium)  # This should not exceed Enterprise's capacity
-enterprise.display_details()
+# # Creating spaceships
+# falcon = Spaceship("Falcon", 50000)
+# apollo = Spaceship("Apollo", 100000)
+# enterprise = Spaceship("Enterprise", 220000)
+# print("1.===================================")
+# # Creating cargo
+# gold = Cargo("Gold", 20000)
+# platinum = Cargo("Platinum", 25000)
+# dilithium = Cargo("Dilithium", 50000)
+# trilithium = Cargo("Trilithium", 70000)
+# neutronium = Cargo("Neutronium", 80000)
+# print("2.===================================")
+# # Loading cargo onto spaceships
+# falcon.load_cargo(gold)
+# falcon.load_cargo(platinum)
+# falcon.display_details()
+# print("3.===================================")
+# apollo.load_cargo(gold)  # Apollo will not reach its total capacity
+# apollo.display_details()
+# print("4.===================================")
+# falcon.load_cargo(neutronium)  # This should exceed Falcon's capacity
+# print("5.===================================")
+# enterprise.load_cargo(dilithium)
+# enterprise.load_cargo(trilithium)
+# enterprise.load_cargo(neutronium)  # This should not exceed Enterprise's capacity
+# enterprise.display_details()
+
+
