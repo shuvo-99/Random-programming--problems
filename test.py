325f9cd2f59704a408336d93a19936b6cb83ece1
Md. Anonto Shuvo <anontoshuvo99@gmail.com>
2023-12-12 10:03:22 +0600

    Update test.py

--- test.py
+++ test.py
@@ -4686,3 +4686,50 @@
 # fixed_account2.print_details()
 # print("-----------16------------")
 # fixed_account2.withdraw(10000)
+
+# class Shape:
+
+#   def __init__(self, name='Default', height=0, base=0):
+#     self.area = 0
+#     self.name = name
+#     self.height = height
+#     self.base = base
+
+#   def get_height_base(self):
+#     return "Height: "+str(self.height)+",Base: "+str(self.base)
+
+# class triangle(Shape):
+#     def __init__(self, n='Default', h=0, b=0):
+#         super().__init__(n, h, b)
+#     def calcArea(self):
+#         return .5*self.base*self.height
+#     def printDetail(self):
+#         str1 = ''
+#         str1 += f"Shape name: {self.name}\n"
+#         str1 += f"{self.get_height_base()}\n"
+#         str1 += f"Area: {self.calcArea()}"
+#         return str1
+    
+# class trapezoid(Shape):
+#     def __init__(self, n, h,b, side_a):
+#         super().__init__(n, h, b)
+#         self.side_a = side_a
+    
+#     def calcArea(self):
+#        self.area = ((self.side_a+self.base)/2)*10
+
+#     def printDetail(self):
+#        print(self.area)
+# #write your code here
+
+# tri_default = triangle()
+# tri_default.calcArea()
+# print(tri_default.printDetail())
+# print('--------------------------')
+# tri = triangle('Triangle', 10, 5)
+# tri.calcArea()
+# print(tri.printDetail())
+# print('---------------------------')
+# trap = trapezoid('Trapezoid', 10, 6, 4)
+# trap.calcArea()
+# print(trap.printDetail())
